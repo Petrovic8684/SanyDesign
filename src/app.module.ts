1ff2a5881fcded5f4bab3bc0b48ef06f035966a1
Petrovic8684 <jovan8684@gmail.com>
2025-08-15 16:29:14 +0200

    Fixed vercel deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,11 @@ import { ToolModule } from './tool/tool.module';
 import { AuthModule } from './auth/auth.module';
 import { MessageModule } from './message/message.module';
 import { FaqModule } from './faq/faq.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ProjectModule, ServiceModule, ToolModule, AuthModule, MessageModule, FaqModule],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
